1f08e97704301da5932cc5f3e497c2d303bf1106
Athanasios Kottaridis <tkottaridis@cognity.gr>
2023-02-04 19:25:05 +0200

    Implementation: create events cells and add assets to project

---
+++ Domain/Domain/Utils/Extensions/Collection_Ext.swift
@@ -0,0 +1,8 @@
+//
+//  Collections_Ext.swift
+//  Domain
+//
+//  Created by thanos kottaridis on 4/2/23.
+//
+
+import Foundation
---
+++ Sports/Sports/Controllers/SportsLanding/Components/EventCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  EventCollectionViewCell.swift
+//  Sports
+//
+//  Created by thanos kottaridis on 4/2/23.
+//
+
+import UIKit
+
+class EventCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
